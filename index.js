2e1a46ded9878bcbd4a598a5d71c059c72deabdb
jkzasori <jotafu@live.com>
2020-06-08 00:23:57 -0500

    MOdulo sencillo con saludos creado

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = {
+    hola: function() {
+        console.log('¡Hola Mundo!');
+    },
+    holaPersonalizado: function(nombre) {
+        console.log(`Hola ${ nombre }`);
+    },
+    holaAlCursoNPM: function() {
+        console.log('Hola al Curso NPM');
+    }
+}
